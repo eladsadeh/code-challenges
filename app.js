4af2d2f0b26de0f92ef6c821589a9b303694634f
eladsadeh <elad.sadeh1@.gmail.com>
2022-01-15 11:55:52 -0500

    likes statement

--- app.js
+++ app.js
@@ -35,27 +35,54 @@ function tribonacci(signature, n) {
 
 function XO(string) {
 	const arr = string.split('');
-	return arr.filter((c) => c.toLowerCase() === 'x').length
-	=== arr.filter((c) => c.toLowerCase() === 'o').length;
+	return (
+		arr.filter((c) => c.toLowerCase() === 'x').length ===
+		arr.filter((c) => c.toLowerCase() === 'o').length
+	);
 }
 
 // console.log(XO('zzzbbb'));
-
 
 // Given an array of ones and zeroes, convert the equivalent binary value to an integer.
 // Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
 
 function binaryToNum(arr) {
-	console.log(arr.reduce((acc,val,idx) => acc + val * 2 ** (arr.length - idx -1),0));
+	console.log(
+		arr.reduce((acc, val, idx) => acc + val * 2 ** (arr.length - idx - 1), 0)
+	);
 }
+
 const binaryArrayToNumber = (arr) => parseInt(arr.join(''), 2);
 
 // console.log(binaryArrayToNumber([1, 1, 1, 0]));
 
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
 function moveZeros(arr) {
-	console.log(Array(...arr.filter((e) => e !== 0), ...arr.filter((e) => e === 0)));
+	console.log(
+		Array(...arr.filter((e) => e !== 0), ...arr.filter((e) => e === 0))
+	);
 	return arr;
-	
 }
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+// Who likes it?
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+function whoLikesIt(arr) {
+	const answers = [
+		'no one likes this',
+		`${arr[0]} likes this`,
+		`${arr[0]} and ${arr[1]} like this`,
+		`${arr[0]}, ${arr[1]} and ${arr[2]} like this`,
+		`${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`,
+	];
+
+	console.log(answers[arr.length > 4 ? 4 : arr.length]);
+}
+
+whoLikesIt(['Elad', 'Alex', 'Jacob']);
