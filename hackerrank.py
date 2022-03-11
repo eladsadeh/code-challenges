3e7efe50b42615f701ca14c440f158c69d380462
eladsadeh <elad.sadeh1@.gmail.com>
2022-03-11 15:15:35 -0500

    bomber game challenge

--- hackerrank.py
+++ hackerrank.py
@@ -19,7 +19,6 @@ def palindromIndex(s):
                 i += 1
                 j -= 2
             else:
-                # the right side can't be palindrom
                 # try the left side
                 break
         else:
@@ -30,7 +29,7 @@ def palindromIndex(s):
     if (found is True and i >= j):
         print(f'right: {i}')
         return idx
-    # Try the other side
+    # Else, try the other side
     found = False
 
     b = len(s)-a-1
@@ -51,51 +50,3 @@ def palindromIndex(s):
     return idx
 
 print(palindromIndex('baa'))
-'''
-found = False
-    # if its a palindrom return -1
-    if s == s[::-1]: 
-        return -1
-    # check the left side
-    i = 0
-    j = len(s)-1
-    while(i < j):
-        if s[i] == s[j]:
-            i += 1
-            j -= 1
-        elif found is True:
-            # if idx already found its more than one character
-            break
-        elif s[i] == s[j-1]:
-            found = True
-            idx = j
-            i += 1
-            j -= 2
-        else:
-            # more than one characters are not the same
-            break
-    # if the foreign character was found and the whole string was checked return the index
-    if (found is True and i >= j): 
-        return idx
-        
-    # Try the other side
-    found = False
-    i = 0
-    j = len(s)-1
-    while(i < j):
-        if s[i] == s[j]:
-            i += 1
-            j -= 1
-        elif found is True:
-            # if idx already found its more than one character
-            return -1
-        elif s[i+1] == s[j]:
-            idx = i
-            i += 2
-            j -= 1
-        else:
-            return -1
-    return idx
-'''
-
-const numbers 
