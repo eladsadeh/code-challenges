3d9a39a692d6cdf14ae4a07b07d5469580d34f57
eladsadeh <elad.sadeh1@.gmail.com>
2022-05-03 11:03:05 -0400

    clean up

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,10 +1,15 @@
 function arrayManipulation(n, queries) {
-	const arr = new Array(n).fill(0)
-    queries.forEach(([a,b,k]) => {
-        arr[a-1] += k
-        if(b < n) arr[b] -= k
-    })
-   console.log(arr.reduce(([c,mx],el) => [c += el, c > mx ? c : mx],[0,0])[1])
+	const arr = new Array(n).fill(0);
+	queries.forEach(([a, b, k]) => {
+		arr[a - 1] += k;
+		if (b < n) arr[b] -= k;
+	});
+	console.log(
+		arr.reduce(([c, mx], el) => [(c += el), c > mx ? c : mx], [0, 0])[1]
+	);
 }
 
-arrayManipulation(10, [[1,5 ,3],[4, 8, 5]] )
+arrayManipulation(10, [
+	[1, 5, 3],
+	[4, 8, 5],
+]);
