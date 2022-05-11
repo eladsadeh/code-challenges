707a3d5444114b198b48f1a125ce40036537f373
eladsadeh <elad.sadeh1@.gmail.com>
2022-05-11 19:14:24 -0400

    SQL challenges

--- matchingStrings.py
+++ matchingStrings.py
@@ -1,8 +1,13 @@
 def matchingStrings(strings, queries):
     res = []
+    res2= []
     for q in queries:
         res.append(strings.count(q))
+
+    for q in queries:
+        s = filter(lambda x: x == q, strings)
+        res2.append(len(list(s)))
     
-    print(res)
+    print(res, res2)
 
 matchingStrings(['aba', 'baba', 'aba', 'xzxb'], ['aba', 'xzxb', 'ab'])
--- query.sql
+++ query.sql
@@ -6,3 +6,15 @@
 SELECT concat(NAME,concat("(",concat(substr(OCCUPATION,1,1),")"))) FROM OCCUPATIONS ORDER BY NAME ASC;
 
 SELECT "There are a total of ", count(OCCUPATION), concat(lower(occupation),"s.") FROM OCCUPATIONS GROUP BY OCCUPATION ORDER BY count(OCCUPATION), OCCUPATION ASC
+
+-- Query a list of CITY names from STATION for cities that have an even ID number. where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+-- Print the results in any order, but exclude duplicates from the answer.
+select distinct city from station where lat_n > 0 and long_w > 0 and MOD(id, 2) = 0;
+
+-- Difference between total number of cities and unique cities
+SELECT (COUNT(CITY) - COUNT(DISTINCT CITY)) FROM STATION where lat_n > 0 and long_w > 0;
+
+-- Print the cities with shortest and longest names sorted alphabethically
+select city, length(city) from station order by length(city),city asc limit 1;
+select city, length(city) from station order by length(city) desc limit 1;
