2c069a3bd54024db00fb4cd50cdc24248e1e45de
eladsadeh <elad.sadeh1@.gmail.com>
2022-05-19 14:03:45 -0400

    longest sub-string in python and javascript

---
+++ longestSubString.js
@@ -0,0 +1,21 @@
+var lengthOfLongestSubstring = function (s) {
+	if (s === '') return 0;
+	cMap = {};
+	p = -1;
+	max = 0;
+	for (let i = 0; i < s.length; i++) {
+		c = s[i];
+		if (cMap[c] !== undefined && p < cMap[c]) {
+			// if c in the current substring, update the pointer
+			p = cMap[c];
+		} else {
+			// else, update max
+			max = max < i - p ? i - p : max;
+		}
+		cMap[c] = i;
+	}
+
+	return max;
+};
+
+console.log(lengthOfLongestSubstring(''));
