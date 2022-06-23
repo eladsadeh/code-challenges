b1a74a77e47874e85df7f41e660482d8cfce8e5a
eladsadeh <elad.sadeh1@.gmail.com>
2022-06-23 14:51:28 -0400

    http requests in python and nodejs

---
+++ python/http_request.py
@@ -0,0 +1,17 @@
+
+import json
+from urllib.request import urlopen
+
+def send_request(key):
+    base_url = 'https://jsonmock.hackerrank.com/api/moviesdata/search/?Title='
+    api_url = f'{base_url}{key}'
+    print(api_url)
+    # body = urlopen(api_url).read()
+    with urlopen(api_url) as response:
+        body = response.read()
+
+    data = json.loads(body)
+
+    print(data['data'])
+
+send_request('maze')
