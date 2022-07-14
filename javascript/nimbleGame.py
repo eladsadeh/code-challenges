ed310a4f2fb03bc0f51dd54343e7a2762488068e
eladsadeh <elad.sadeh1@.gmail.com>
2022-07-14 17:08:10 -0400

    add explenation

--- javascript/nimbleGame.py
+++ javascript/nimbleGame.py
@@ -1,5 +1,12 @@
 # https://www.hackerrank.com/challenges/nimble-game-1/problem
+# Firstly consider, that any even number of coins on the same square can be removed
+# If first player wants to maintain the state of the game, he can simply copy second
+# player's moves and vice versa  
 
+# Next, we convert this game into a normal "Nim Game", think of the number of spaces 
+# that can be removed as the number of objects in a pile.  i.e. a coin on square 3 
+# can be moved up to 3 spaces over and you can choose how many spaces 1, 2 or 3 to 
+# remove from the "pile"
 def nimbleGame(s):
     # count = sum(s[1:])
     # return 'First' if count%2 else 'Second'
