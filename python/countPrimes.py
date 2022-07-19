9f7c1cef0e1f6aaa1148356a4f0d6199206a0c96
eladsadeh <elad.sadeh1@.gmail.com>
2022-07-19 09:09:06 -0400

    Alice and Bob game

--- python/countPrimes.py
+++ python/countPrimes.py
@@ -1,20 +1,33 @@
-def countPrimes(n):
+def findPrimes(n):
     if n == 1:
         return 'Bob'
 
-    def isPrime(m):
+    # def isPrime(m): 
+    #     for num in primes:
+    #         if m%num == 0:
+    #             return False
+    #     return True
     
-        for num in primes:
-            if m%num == 0:
-                return False
-        return True
-    
-    primes = [2]
+    is_prime = [1] * (n+1)
+    num =2
+    count = 0
+    while num <= n:
+    # for num in range(2,n//2+1):
+        if is_prime[num]:
+            count += 1
+            for i in range(num*num,n+1,num):
+                is_prime[i] = 0
+        num += 1
 
-    for i in range (2,n+1):
-        if isPrime(i):
-            primes.append(i)
-    print(primes)
-    return len(primes)
+    print(is_prime)
+    print(is_prime[2:].count(1))
+    print(count)
 
-print(countPrimes(1))
+    # primes = [2]
+    # for i in range (2,n+1):
+    #     if isPrime(i):
+    #         primes.append(i)
+    # print(primes)
+    # return len(primes)
+
+print(findPrimes(11))
